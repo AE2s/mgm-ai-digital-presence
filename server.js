84869e37e2a1652787d9b404c52a66e091b5bdb5
ESSAGHIR Anas (External) <anas.essaghir.ext@loccitane.com>
2024-12-11 18:04:50 +0100

    change server.js

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const productRoutes = require('./routes/productRoutes.js');
-const { computeData } = require('./models/models.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const productRoutes = require("./routes/productRoutes.js");
+const { computeData } = require("./models/models.js");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
-    app.post('/test', async (req, res) => {
-        res.json(await computeData(req.body));
-    } );
+// app.all("/*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
+app.post("/test", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.json(await computeData(req.body));
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Internal server error' });
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal server error" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
