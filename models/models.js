cdf3939d5d9384c7c557815d7407279ab7d2d2b2
Thomas Pasquet <thomas.pasquet@loccitane.com>
2024-12-11 14:22:10 +0100

    add computeData function to module models

---
+++ models/models.js
@@ -0,0 +1,3 @@
+module.exports = {
+    computeData: () => 'data',
+};
--- tests/models.test.js
+++ tests/models.test.js
@@ -1,4 +1,9 @@
-test('models exist', () => {
-    const models = require('../models/models');
+const models = require('../models/models');
+
+test('#1 models exist', () => {
     expect(models).toBeDefined();
+});
+
+test('#2 models contains computeData function', () => {
+    expect(models.computeData).toBeDefined();
 });
